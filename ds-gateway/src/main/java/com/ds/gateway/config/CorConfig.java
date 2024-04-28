b6b39720dd7659e97b9bc7b557b78dc1805b2c80
Writiger <75297838+writiger@users.noreply.github.com>
2024-04-28 14:41:39 +0800

    解决跨域问题

---
+++ ds-gateway/src/main/java/com/ds/gateway/config/CorConfig.java
@@ -0,0 +1,28 @@
+package com.ds.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author writiger
+ * @description 跨域配置
+ * @create_at 2024-04-28 14:36
+ */
+@Configuration
+public class CorConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
